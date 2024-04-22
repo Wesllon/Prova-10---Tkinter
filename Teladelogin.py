f1815c6fc5812fb052fcc22c212944ad67216cc0
Wesllon Dev <115670865+Wesllon@users.noreply.github.com>
2024-04-22 01:02:24 -0300

    Update Teladelogin.py

--- Teladelogin.py
+++ Teladelogin.py
@@ -1,4 +1,6 @@
-# Usando seus conhecimentos aprendidos em sala, realize uma interface de login utilizando a biblioteca Tkinter em Python. O objetivo é permitir que o usuário faça login somente se a senha tiver mais de 6 dígitos e se o e-mail contiver o caractere "@", ou seja, realizar uma tela de login com restrições de e-mail e senha.
+# Usando seus conhecimentos aprendidos em sala, realize uma interface de login utilizando a biblioteca Tkinter em Python. O objetivo é permitir que o usuário faça login 
+# somente se a senha tiver mais de 6 dígitos e se o e-mail contiver o caractere "@", ou seja, realizar uma tela de login com restrições de e-mail e senha.
+
 from tkinter import *
 
 # Função para realizar o login
